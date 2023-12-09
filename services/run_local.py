df29d31b556f242013d1b8acbf37838173b81dae
Ledger <ledger@Ledgers-MacBook-Pro.local>
2023-12-09 18:37:27 -0500

    feat: add serverless offline multi to run all services at once for local dev

---
+++ services/run_local.py
@@ -0,0 +1,20 @@
+import subprocess
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--stage', default='dev', type=str)
+    args, _ = parser.parse_known_args()
+
+    base = "serverless-offline-multi"
+    api = "--directory api --port 3005 --watch true"
+    data = "--directory data --port 3006 --watch true"
+    task = "--directory task --port 3007 --watch true"
+    backend_jobs = "--directory backend-jobs --port 3008 --watch true"
+    stage = f"--stage {args.stage}"
+
+    cmd = ' '.join([base, api, data, task, backend_jobs, stage])
+    #print(cmd)
+
+    subprocess.run(cmd, shell=True) 
