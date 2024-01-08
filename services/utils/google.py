7c6a9a9c246129c7035b819ed78f5292a1032e80
Ledger <ledger@Ledgers-MacBook-Pro.local>
2024-01-08 14:44:06 -0500

    ops: rm tiktoken from api reqs

--- services/utils/google.py
+++ services/utils/google.py
@@ -10,9 +10,7 @@ import csv
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
 
 
